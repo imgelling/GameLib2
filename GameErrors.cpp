cc2095742b07f955f2739d8ecaa49084dc2e9c95
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-08-30 18:27:57 -0400

    Small change with << overload

--- GameErrors.cpp
+++ GameErrors.cpp
@@ -6,22 +6,4 @@ namespace game
 	{
 		return  (lastErrorType != GameErrors::None);
 	}
-
-	std::ostream& operator<< (std::ostream& stream , const GameError error)
-	{
-		uint32_t errID = (uint32_t)error.lastErrorType;
-		switch (errID)
-		{
-		case (uint32_t)GameErrors::None: stream << "GameErrorNone"; break;
-		case (uint32_t)GameErrors::GameMemoryAllocation: stream << "GameErrorMemoryAllocation"; break;
-		case (uint32_t)GameErrors::GameInvalidParameter: stream << "GameErrorInvalidParameters"; break;
-		case (uint32_t)GameErrors::GameWindowsSpecific: stream << "GameErrorWindowsSpecific"; break;
-		case (uint32_t)GameErrors::GameOpenGLSpecific: stream << "GameErrorOpenGLSpecific"; break;
-		case (uint32_t)GameErrors::GameVulkanSpecific: stream << "GameErrorVulkanSpecific"; break;
-		case (uint32_t)GameErrors::GameRenderer: stream << "GameRenderer"; break;
-		default: return stream << "GameErrorUnknown";
-		}
-		// Append the error text to general error code
-		return stream << " : " << error.lastErrorString << "\n";
-	}
 }
