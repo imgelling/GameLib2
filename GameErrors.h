bd557ff448fa1eab121bceb6186bae5510bb25d5
ImGelling <47453119+imgelling@users.noreply.github.com>
2022-09-09 19:48:13 -0400

    Update GameErrors.h

--- GameErrors.h
+++ GameErrors.h
@@ -22,22 +22,22 @@ namespace game
 		std::string lastErrorString = "None";
 		explicit operator bool() const;
 		//friend std::ostream& operator<< (std::ostream& stream, const game::GameError error);
-		friend std::ostream& operator<< (std::ostream& stream, const GameError error)
+		friend std::ostream& operator<< (std::ostream& _stream, const GameError error)
 		{
 			uint32_t errID = (uint32_t)error.lastErrorType;
 			switch (errID)
 			{
-			case (uint32_t)GameErrors::None: stream << "GameErrorNone"; break;
-			case (uint32_t)GameErrors::GameMemoryAllocation: stream << "GameErrorMemoryAllocation"; break;
-			case (uint32_t)GameErrors::GameInvalidParameter: stream << "GameErrorInvalidParameters"; break;
-			case (uint32_t)GameErrors::GameWindowsSpecific: stream << "GameErrorWindowsSpecific"; break;
-			case (uint32_t)GameErrors::GameOpenGLSpecific: stream << "GameErrorOpenGLSpecific"; break;
-			case (uint32_t)GameErrors::GameVulkanSpecific: stream << "GameErrorVulkanSpecific"; break;
-			case (uint32_t)GameErrors::GameRenderer: stream << "GameRenderer"; break;
-			default: return stream << "GameErrorUnknown";
+			case (uint32_t)GameErrors::None: _stream << "GameErrorNone"; break;
+			case (uint32_t)GameErrors::GameMemoryAllocation: _stream << "GameErrorMemoryAllocation"; break;
+			case (uint32_t)GameErrors::GameInvalidParameter: _stream << "GameErrorInvalidParameters"; break;
+			case (uint32_t)GameErrors::GameWindowsSpecific: _stream << "GameErrorWindowsSpecific"; break;
+			case (uint32_t)GameErrors::GameOpenGLSpecific: _stream << "GameErrorOpenGLSpecific"; break;
+			case (uint32_t)GameErrors::GameVulkanSpecific: _stream << "GameErrorVulkanSpecific"; break;
+			case (uint32_t)GameErrors::GameRenderer: _stream << "GameRenderer"; break;
+			default: return _stream << "GameErrorUnknown";
 			}
 			// Append the error text to general error code
-			return stream << " : " << error.lastErrorString << "\n";
+			return _stream << " : " << error.lastErrorString << "\n";
 		}
 	};
 
