abeb0fde4533ac99dd26f364a5359a22aa8db1f6
ImGelling <47453119+imgelling@users.noreply.github.com>
2024-01-03 20:10:17 -0500

    Vector3 added

--- GameMath.h
+++ GameMath.h
@@ -61,6 +61,162 @@ namespace game
 	typedef Vector2<double> Vector2d, Pointd;
 #pragma endregion
 
+#pragma region Vector3
+	template <typename T>
+	class Vector3
+	{
+	public:
+		T x = 0;
+		T y = 0;
+		T z = 0;
+		T w = 1;
+		Vector3() {}
+		Vector3(const T& x, const T& y, const T& z)
+		{
+			this->x = x;
+			this->y = y;
+			this->z = z;
+			this->w = (T)1;
+		}
+		Vector3 operator+ (const Vector3& rhs)
+		{
+			Vector3<T> c;
+			c.x = x + rhs.x;
+			c.y = y + rhs.y;
+			c.z = z + rhs.z;
+			return c;
+		}
+		Vector3& operator+= (const Vector3& rhs)
+		{
+			x = x + rhs.x;
+			y = y + rhs.y;
+			z = z + rhs.z;
+			return *this;
+		}
+		Vector3 operator- (const Vector3& rhs)
+		{
+			Vector3<T> c;
+			c.x = x - rhs.x;
+			c.y = y - rhs.y;
+			c.z = z - rhs.z;
+			return c;
+		}
+		Vector3& operator-= (const Vector3& rhs)
+		{
+			x = x - rhs.x;
+			y = y - rhs.y;
+			z = z - rhs.z;
+			return *this;
+		}
+		Vector3 operator* (const T& scalar)
+		{
+			Vector3<T> c;
+			c.x = x * scalar;
+			c.y = y * scalar;
+			c.z = z * scalar;
+			return c;
+		}
+		Vector3& operator*= (const T& scalar)
+		{
+			x = x * scalar;
+			y = y * scalar;
+			z = z * scalar;
+			return *this;
+		}
+		Vector3 operator/ (const T& scalar)
+		{
+			Vector3<T> c;
+			c.x = x / scalar;
+			c.y = y / scalar;
+			c.z = z / scalar;
+			return c;
+		}
+		Vector3& operator/= (const T& scalar)
+		{
+			x = x / scalar;
+			y = y / scalar;
+			z = z / scalar;
+			return *this;
+		}
+		//Vector3 operator* (const Matrix4x4<T>& mat)
+		//{
+		//	Vector3<T> ret;
+		//	ret.x = (x * mat.m[0] + y * mat.m[4] + z * mat.m[8] + w * mat.m[12]);
+		//	ret.y = (x * mat.m[1] + y * mat.m[5] + z * mat.m[9] + w * mat.m[13]);
+		//	ret.z = (x * mat.m[2] + y * mat.m[6] + z * mat.m[10] + w * mat.m[14]);
+		//	ret.w = (x * mat.m[3] + y * mat.m[7] + z * mat.m[11] + w * mat.m[15]);
+		//	//if (w != 0)
+		//	//{
+		//	//	ret.x /= w;
+		//	//	ret.y /= w;
+		//	//	ret.z /= w;
+		//	//}
+		//	return ret;
+		//}
+		//Vector3& operator*= (const Matrix4x4<T>& mat)
+		//{
+		//	Vector3<T> ret;
+		//	ret.x = (x * mat.m[0] + y * mat.m[4] + z * mat.m[8] + w * mat.m[12]);
+		//	ret.y = (x * mat.m[1] + y * mat.m[5] + z * mat.m[9] + w * mat.m[13]);
+		//	ret.z = (x * mat.m[2] + y * mat.m[6] + z * mat.m[10] + w * mat.m[14]);
+		//	ret.w = (x * mat.m[3] + y * mat.m[7] + z * mat.m[11] + w * mat.m[15]);
+		//	*this = ret;
+		//	return *this;
+		//}
+		float Mag2() { return ((x * x) + (y * y) + (z * z)); }
+		float Mag() { return sqrt((x * x) + (y * y) + (z * z)); }
+		float Dot(const Vector3<T>& rhs) {
+			return (x * rhs.x) + (y * rhs.y) + (z * rhs.z);
+		}
+		void Normalize()
+		{
+			float l = Mag();
+			if (l == 0) return;
+			x /= l;
+			y /= l;
+			z /= l;
+		}
+		Vector3 Cross(const Vector3& rhs)
+		{
+			Vector3<T> t;
+			t.x = (y * rhs.z) - (z * rhs.y);
+			t.y = (z * rhs.x) - (x * rhs.z);
+			t.z = (x * rhs.y) - (y * rhs.x);
+			return t;
+		}
+		Vector3 Lerp(const Vector3<T>& second, const T by)
+		{
+			return *this * (1 - by) + (Vector3<T>)second * by;
+		}
+		Vector3 Lerp(const Vector3<T>& first, const Vector3<T>& second, const T by)
+		{
+			Vector3<T> ret;
+			ret.x = first.x * (1.0 - by) + second.x * by;
+			ret.y = first.y * (1.0 - by) + second.y * by;
+			ret.z = first.z * (1.0 - by) + second.z * by;
+			return ret;
+		}
+		Vector3 NLerp(const Vector3<T>& second, const T& by)
+		{
+			return Lerp(second, by).Normalize();
+		}
+		Vector3 SLerp(const Vector3<T>& second, const T& by)
+		{
+			double dot = Dot(second);
+			if (dot < -1.0) dot = -1.0;
+			else if (dot > 1.0) dot = 1.0;
+			double theta = std::acos(dot) * by;
+			Vector3 relative = (Vector3<T>)second - (*this * by);
+			relative = relative.Normalize();
+			return ((*this * std::cos(theta)) + (relative * std::sin(theta)));
+		}
+	private:
+	};
+	typedef Vector3<float> Vector3f;
+	typedef Vector3<double> Vector3d;
+	typedef Vector3<int> Vector3i;
+#pragma endregion
+
 #pragma region Rect
 	template <typename T>
 	class Rect
