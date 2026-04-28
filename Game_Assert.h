32a650b93349fc3be5a12b63c910f3eacb565987
ImGelling <47453119+imgelling@users.noreply.github.com>
2026-04-28 15:35:08 -0400

    Create Game_Assert.h

---
+++ Game_Assert.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#if defined(_DEBUG)
+#define GAME_ASSERT(condition)                                      \
+    do																\
+	{                                                               \
+        if (!(condition))                                           \
+		{                                                           \
+            __debugbreak();                                         \
+        }                                                           \
+    } while (false)
+#else
+#define GAME_ASSERT(condition)
+#endif
