1b49229456226346dff15eced154ea50c8975ce5
ImGelling <47453119+imgelling@users.noreply.github.com>
2026-07-14 17:25:06 -0400

    can use X by Y sprites, with margins

--- GameSpriteSheet.h
+++ GameSpriteSheet.h
@@ -13,11 +13,11 @@ namespace game
 	{
 	public:
 		SpriteSheet();
-		SpriteSheet(const Texture2D& texture, const uint32_t width, const uint32_t height);
-		void Initialize(const Texture2D &texture, const uint32_t width, const uint32_t height) noexcept;
-		void Initialize(SpriteSubSheet& subSheet, const std::string& subName, const uint32_t width, const uint32_t height);
-		void Initialize(const Texture2D &texture, const Pointi &size) noexcept;
-		void Initialize(SpriteSubSheet& subSheet, const std::string& subName, const Pointi& size);
+		SpriteSheet(const Texture2D& texture, const uint32_t width, const uint32_t height, const uint32_t marginX = 0, const uint32_t marginY = 0);
+		void Initialize(const Texture2D &texture, const uint32_t width, const uint32_t height, const uint32_t marginX = 0, const uint32_t marginY = 0) noexcept;
+		void Initialize(SpriteSubSheet& subSheet, const std::string& subName, const uint32_t width, const uint32_t height, const uint32_t marginX = 0, const uint32_t marginY = 0);
+		void Initialize(const Texture2D& texture, const Pointi& size, const game::Pointi &margin = { 0,0 }) noexcept;
+		void Initialize(SpriteSubSheet& subSheet, const std::string& subName, const Pointi& size, const game::Pointi &margin = { 0,0 });
 		Recti GetRectFromId(const int32_t id) const noexcept;
 		uint32_t tileWidth;
 		uint32_t tileHeight;
@@ -28,6 +28,7 @@ namespace game
 	private:
 		uint32_t _tilesPerRow;
 		Pointi _textureOffset;
+		Pointi _margins;
 	};
 
 	//void SpriteSheet::SetTilesPerRow(const uint32_t tiles)
@@ -57,7 +58,7 @@ namespace game
 		_tilesPerRow = 0;
 	}
 	
-	SpriteSheet::SpriteSheet(const Texture2D& texture, const uint32_t width, const uint32_t height)
+	SpriteSheet::SpriteSheet(const Texture2D& texture, const uint32_t width, const uint32_t height, const uint32_t marginX, const uint32_t marginY)
 	{
 		SpriteSheet();
 		GAME_ASSERT(width);
@@ -65,42 +66,51 @@ namespace game
 		//if (!width || !height) return;
 		tileWidth = width;
 		tileHeight = height;
-		_tilesPerRow = texture.width / tileWidth;
+		_tilesPerRow = texture.width / (tileWidth + marginX);
+		_margins.x = marginX;
+		_margins.y = marginY;
 	}
 
-	void SpriteSheet::Initialize(SpriteSubSheet& subSheet, const std::string& subName, const Pointi& size)
+	void SpriteSheet::Initialize(SpriteSubSheet& subSheet, const std::string& subName, const Pointi& size, const game::Pointi &margin)
 	{
-		Initialize(subSheet, subName, size.width, size.height);
+		Initialize(subSheet, subName, size.width, size.height, margin.x, margin.y);
 	}
 
-	void SpriteSheet::Initialize(SpriteSubSheet& subSheet, const std::string& subName, const uint32_t width, const uint32_t height)
+	void SpriteSheet::Initialize(SpriteSubSheet& subSheet, const std::string& subName, const uint32_t width, const uint32_t height, const uint32_t marginX, const uint32_t marginY)
 	{
 		GAME_ASSERT(!(subSheet.subTextureRegistry.find(subName) == subSheet.subTextureRegistry.end()));
-
+		GAME_ASSERT(width);
+		GAME_ASSERT(height);
 		tileWidth = width;
 		tileHeight = height;
 		//int32_t textureWidth = subSheet.subTextureRegistry[subName].right;
-		_tilesPerRow = subSheet.subTextureRegistry[subName].right / tileWidth;
+		_tilesPerRow = subSheet.subTextureRegistry[subName].right / (tileWidth + marginX);
+		_margins.x = marginX;
+		_margins.y = marginY;
 	}
 
-	inline void SpriteSheet::Initialize(const Texture2D &texture, const uint32_t width, const uint32_t height) noexcept
+	inline void SpriteSheet::Initialize(const Texture2D &texture, const uint32_t width, const uint32_t height, const uint32_t marginX, const uint32_t marginY) noexcept
 	{
+		GAME_ASSERT(width);
+		GAME_ASSERT(height);
 		tileWidth = width;
 		tileHeight = height;
-		_tilesPerRow = texture.width / tileWidth;
+		_tilesPerRow = texture.width / (tileWidth+marginX);
+		_margins.x = marginX;
+		_margins.y = marginY;
 	}
 
-	inline void SpriteSheet::Initialize(const Texture2D &texture, const Pointi &size) noexcept
+	inline void SpriteSheet::Initialize(const Texture2D &texture, const Pointi &size, const game::Pointi &margin) noexcept
 	{
-		Initialize(texture, size.width, size.height);
+		Initialize(texture, size.width, size.height, margin.x, margin.y);
 	}
 
 	inline Recti SpriteSheet::GetRectFromId(const int32_t id) const noexcept
 	{
 		Recti rectangle;
 
-		rectangle.top = (id / _tilesPerRow * tileHeight) + _textureOffset.y;
-		rectangle.left = (id % _tilesPerRow * tileWidth) + _textureOffset.x;
+		rectangle.top = (id / _tilesPerRow * (tileHeight + _margins.x)) + _textureOffset.y;
+		rectangle.left = (id % _tilesPerRow * (tileWidth + _margins.y)) + _textureOffset.x;
 		rectangle.bottom = rectangle.top + tileHeight;
 		rectangle.right = rectangle.left + tileWidth;
 
